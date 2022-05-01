87fa69100066cadfee2ad1032f015a87aec972f6
Zhongyang Wu <zhongyang.wu@outlook.com>
2022-05-01 18:52:34 -0400

    add json format of sampling types.

--- grpc-type-generator/src/main.rs
+++ grpc-type-generator/src/main.rs
@@ -1,22 +1,34 @@
-use std::error::Error;
-use std::io;
 use protobuf_codegen::Customize;
 use protoc_grpcio::compile_grpc_protos;
+use std::error::Error;
+use std::io;
+
+mod sampling_json;
 
 fn main() -> Result<(), io::Error> {
-    compile_grpc_protos(
-        &[
-            "src/sampling.proto"
-        ],
-        &["src/"],
-        "src/",
-        Some(Customize {
-            expose_fields: Some(true),
-            serde_derive: Some(true),
-            ..Default::default()
-        }),
-    )
-        .expect("Error generating protobuf");
+    // prost_build::compile_protos(&["src/sampling.proto"], &["src/"]).unwrap();
+    // compile_grpc_protos(
+    //     &[
+    //         "src/sampling.proto"
+    //     ],
+    //     &["src/"],
+    //     "src/",
+    //     Some(Customize {
+    //         expose_fields: Some(true),
+    //         serde_derive: Some(true),
+    //         ..Default::default()
+    //     }),
+    // )
+    //     .expect("Error generating protobuf");
+    test();
 
     Ok(())
+}
+
+fn test() {
+    let x = r#"
+    {"strategyType":"PROBABILISTIC","probabilisticSampling":{"samplingRate":0.8},"operationSampling":{"defaultSamplingProbability":0.8,"defaultLowerBoundTracesPerSecond":0,"perOperationStrategies":[{"operation":"op1","probabilisticSampling":{"samplingRate":0.2}},{"operation":"op2","probabilisticSampling":{"samplingRate":0.4}},{"operation":"/health","probabilisticSampling":{"samplingRate":0}},{"operation":"/metrics","probabilisticSampling":{"samplingRate":0}}],"defaultUpperBoundTracesPerSecond":0}}
+    "#;
+    let test: sampling_json::SamplingStrategyResponse = serde_json::from_str(x).unwrap();
+    print!("{}", test.strategy_type == sampling_json::SamplingStrategyType::Probabilistic)
 }
